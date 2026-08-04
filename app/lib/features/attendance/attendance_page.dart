53514327de8cfe9bc7f9fe3d0868f874b2da9eb3
ice-kele <1146907147@qq.com>
2026-08-04 12:22:41 +0800

    feat: app/lib/features/attendance/attendance_page.dart

---
+++ app/lib/features/attendance/attendance_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AttendancePage extends StatelessWidget {
+  const AttendancePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('打卡记录')),
+      body: const Center(child: Text('打卡记录页面 · 开发中')),
+    );
+  }
+}
